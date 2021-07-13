a55e76c451e2e305a14152bcf4cc264cd3aeb03f
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-07-13 14:57:57 +0300

    Add support for list of urls

--- ArgumentHandler.py
+++ ArgumentHandler.py
@@ -5,10 +5,13 @@ class ArgumentHandler:
 
     def __init__(self):
         self.printBanner()
-        parser = self.ConfigureParser()
-        args = parser.parse_args()
+        self.parser = self.ConfigureParser()
+        args = self.parser.parse_args()
         self.verbosity = args.verbose
-        self.url = args.url
+        if args.input_url:
+            self.url = args.input_url
+        else: 
+            self.url = args.input_url_file
         if args.manual_proxy_list:
             self.proxy_list = manual_proxy_list
         elif args.input_proxy_file:
@@ -24,7 +27,8 @@ class ArgumentHandler:
 
     def ConfigureParser(self):   
         parser = argparse.ArgumentParser(prog='LFITester.py', description='LFI Automated tester.', epilog='Proxies in the list must be in the following format: protocol://{proxyip} username:password (newline). If you dont have a authenticated proxy then skip the username:password entry and go for a new line')
-        parser.add_argument('url', help='The url to test. The URL format must be http://[URL]?[something]=')
+        parser.add_argument('-u', '--url', dest="input_url", help='The url to test. The URL format must be http://[URL]?[something]=')
+		parser.add_argument('-L', '--list-URLs', dest="input_url_file", help='Input a list of URLs from an external file. The URLs format must be http://[URL]?[something]=', type=argparse.FileType('r'))
         parser.add_argument('-m', '--set-manual-proxy-list', dest="manual_proxy_list",  help="The proxy urls to add (The script expects proxies without authentication. For authenticated proxies use input files.", nargs='*')
         parser.add_argument('-v', '--verbose', action='count', help='Increase output verbosity', default=0)
         parser.add_argument('-p', '--enable-proxies', dest="enabled_proxies", action='store_true', help="Enable proxy redirection. Default proxies are free and you can change them. If you don't want the default proxies you can supply your own and this option will be overrided! Note that the the proxies will be picked at random for each request")
