797e0e33400481d1d606cab982bc512cd1b0f189
konstantinos Pap <konstantinos@localhost.localdomain>
2021-07-13 08:07:13 -0500

    Addition message

--- LFITester.py
+++ LFITester.py
@@ -19,9 +19,11 @@ def main():
 		print(colored("This script doesn't check for Remote File Inclusion (RFI)", 'blue'))
 		print(colored("If it doesn't show any results that means it didn't find anything!!!", 'blue'))
 		if type(arghandler.url) is not list:
+			print(colored(f"Testing: {arghandler.url}\n\n", 'green'))
 			PayloadManager.Payload(arghandler.url, verbosity=arghandler.verbosity)
 		else:
 			for url in arghandler.url:
+				print(colored(f"Testing: {url}\n\n", 'green'))
 				PayloadManager.Payload(url, verbosity = arghandler.verbosity)
 	except KeyboardInterrupt:
 		print('\nGracefully Exiting...\n')
