9efad91dbdd43d0e2cd63a88a42ff4623e3c7813
Kostasssss <56977882+Kostasssss@users.noreply.github.com>
2021-09-18 14:33:40 +0300

    Changed LFITester to lfitester

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ echo Making LFITester.py executable...
 chmod +x LFITester.py
 
 echo Creating symlink for LFITester.py
-ln -s $(pwd)/LFITester.py /bin/LFITester
+ln -s $(pwd)/LFITester.py /bin/lfitester
 
-echo Everything\'s setup! Simply run LFITester to get started!
+echo Everything\'s setup! Simply run lfitester to get started!
