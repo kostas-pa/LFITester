4096a3014ff65c7121182df9479921b76c18b8af
kostas <56977882+kostas-pa@users.noreply.github.com>
2022-05-07 07:17:54 -0400

    autoinstall requirements and added colored output

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
-echo Making LFITester.py executable...
+yellow="\e[1;33m"
+boldgreen="\033[1;32m"
+nocolor="\033[0m"
+
+echo -e "${yellow}Installing the required packages...${nocolor}"
+pip3 install -r requirements.txt
+
+echo -e "${yellow}Making LFITester.py executable...${nocolor}"
 chmod +x LFITester.py
 
-echo Creating symlink for LFITester.py
+echo -e "${yellow}Creating symlink for LFITester.py...${nocolor}"
 ln -s $(pwd)/LFITester.py /bin/lfitester
 
-echo Everything\'s setup! Simply run lfitester to get started!
+echo -e "${boldgreen}Everything\'s setup! Simply run lfitester to get started!${nocolor}"
